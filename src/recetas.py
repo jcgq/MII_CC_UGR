ae851e6604c284ab8807021c9aa893bbd3bb6fdc
Juan Carlos Gonzalez Quesada <juancarlosgq@correo.ugr.es>
2021-10-10 11:33:05 +0200

    Añadiendo el constructor

--- src/recetas.py
+++ src/recetas.py
@@ -1,2 +1,7 @@
-def class recetas:
+class recetas:
+	def __init__(self, nombreRecetas, alimentos, elaboracion, tiempo):
+		self.nombreRecetas = nombreRecetas
+		self.alimentos = alimentos
+		self.elaboracion = elaboracion
+		self.tiempo = tiempo
 
