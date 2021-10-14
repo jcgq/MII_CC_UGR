0604eb0da6d75d549d132caaf615ef3d9edfe0dc
Juan Carlos Gonzalez Quesada <juancarlosgq@correo.ugr.es>
2021-10-14 23:16:23 +0200

    close #26 Creada la función que permite añadir alimentos desde la API a un JSON manteniendo la estructura

--- src/api.py
+++ src/api.py
@@ -1,6 +1,8 @@
 from ssl import VERIFY_DEFAULT
 from py_edamam import Edamam
 from googletrans import Translator
+import json
+import os
 
 class api:
 	def __init__(self):
@@ -16,21 +18,51 @@ class api:
 	def obtenerValorNutricional(self, alimento):
 		valores = self.apiEdamam.search_food(alimento)['parsed']
 		if not valores:
-			valores = ("No hay un valor")
+			calorias = ("No hay un valor")
 		else:
 			calorias = valores[0]['food']['nutrients']
-			cantidad = valores[0]['quantity']
-			print("La cantidad es " + str(cantidad))
+
 		return calorias
+	
+	def obtenerCategoria(self, alimento):
+		valores = self.apiEdamam.search_food(alimento)['parsed']
+		if not valores:
+			categoria = ("No hay un valor")
+		else:
+			categoria = valores[0]['food']['category']
+		return categoria
 
 	def comprobarExistencia(self, busqueda):
 		return None
 
 
 appi = api()
-alimento = appi.traducirPalabra("kiwi")
-valorNutricional = appi.obtenerValorNutricional(alimento)
-print(valorNutricional)
-#print(e.search_nutrient("1 large apple")['uri'])
-#print(e.search_recipe("onion and chicken"))
-#Calorias,Proteina, Grasa, CarboHidratos, Fibra
+for i in range(0,10):
+	al = input("Dime el alimento ")
+	alimento = appi.traducirPalabra(al)
+	valorNutricional = appi.obtenerValorNutricional(alimento)
+	categoria = appi.obtenerCategoria(alimento)
+
+
+	datos = {
+		'nombre':alimento,
+		'nutrientes':valorNutricional,
+		'categoria':categoria
+	}
+
+	cadena_json = json.dumps(datos)
+	print(cadena_json)
+
+	#Lectura
+	with open('../MII_CC_UGR/json/alimentos.json', 'r') as f:
+		c = f.read()
+
+	datosMod = json.dumps(datos)
+	s = json.loads(c)
+	s.append(datos)
+	sC = json.dumps(s, indent=4)
+
+	#Escritura
+	with open('../MII_CC_UGR/json/alimentos.json', 'w') as f:
+		f.write(sC)
+		f.close()
