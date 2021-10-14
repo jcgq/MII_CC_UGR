95c8de40716ea9af39741dcaefad58ca492ad4ec
Juan Carlos Gonzalez Quesada <juancarlosgq@correo.ugr.es>
2021-10-14 11:59:34 +0200

    Cambios a la clase receta

--- src/recetas.py
+++ src/recetas.py
@@ -1,15 +1,30 @@
-class recetas:
+class Receta:
+
 	def __init__(self, nombreRecetas, alimentos, elaboracion, tiempo):
 		self.nombreRecetas = nombreRecetas
 		self.alimentos = alimentos
 		self.elaboracion = elaboracion
 		self.tiempo = tiempo
 
-	def compararRecetas(self, receta1, receta2):
+	def compararReceta(self, receta1, conjuntoRecetas):
 		return None
 
 	def calcularNivelCalorico(self, receta):
 		return None
 
-	def obtenerReceta(self, interesUsuario):
+	def obtenerRecetas(self, interesUsuario):
 		return None
+
+	def getNombre(self):
+		return self.nombreRecetas
+	def getIngredientes(self):
+		return self.alimentos
+	def getProceso(self):
+		return self.elaboracion
+	def getTiempo(self):
+		return self.tiempo
+
+
+recetaPrueba = Receta("Ajo pollo", "pollo 200gr, pepino 50gr, aguacate 60gr", "Cocinar todo de una manera muy rica", "60 minutos")
+
+print(recetaPrueba.getNombre())
