187876fc9458e06e2e649db37df6680ed723a773
Juan Carlos Gonzalez Quesada <juancarlosgq@correo.ugr.es>
2021-10-17 12:15:42 +0200

    close #29 añado la cabecera de la función validación y la lista de unidades permitidas

--- src/recetas.py
+++ src/recetas.py
@@ -1,8 +1,14 @@
 class Receta:
 
+	unidadesPermitidas = {"gramos", "gr", "cucharada", "cucharadita", "ralladura", "diente", "dientes"}
+	
 	def __init__(self, nombreRecetas, alimentos, elaboracion, tiempo):
 		self.nombreRecetas = nombreRecetas
 		self.alimentos = alimentos
 		self.elaboracion = elaboracion
 		self.tiempo = tiempo
+
+
+	def validarAlimentos(self, receta, unidadesPermitidas):
+		return None
 
