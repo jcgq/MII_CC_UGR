3645233a9e03d14590b1080db1b4deffe1078bb1
Juan Carlos Gonzalez Quesada <juancarlosgq@correo.ugr.es>
2021-10-28 18:14:59 +0200

    close #39 Creamos la función que testeará si la validación 1 de los alimentos tiene el comportamiento deseado

--- test/test.py
+++ test/test.py
@@ -4,6 +4,9 @@ sys.path.insert(1, 'src')
 from recetas import Receta
 
 def test_validarCapacidades():
-    recetilla = Receta("Caracoles", "1 kilo de habichuelas, medio litro de agua, dos ralladuras de melón","Cocinar todo con mucho tranquiilp", 67)
-    assert(Receta.validarAlimentos(recetilla, Receta.unidadesPermitidas) == True)
-
+    recetaCorrecta = Receta("Caracoles", "1 kilo de habichuelas, medio litro de agua, dos ralladuras de melón","Cocinar todo con mucho tranquiilp", 67)
+    assert(Receta.validarAlimentos(recetaCorrecta, Receta.unidadesPermitidas) == True)
+    
+    recetaIncorrecta = Receta("Caracoles", "1 carabinero de habichuelas, medio litro de agua, dos cazuelicas de melón","Cocinar todo con mucho tranquiilp", 67)
+    assert(Receta.validarAlimentos(recetaIncorrecta, Receta.unidadesPermitidas) == False)   
+    
