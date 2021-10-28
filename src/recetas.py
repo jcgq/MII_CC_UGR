5f728aef94f66b2caf70aed3cd5b7a27a3d9fed3
Juan Carlos Gonzalez Quesada <juancarlosgq@correo.ugr.es>
2021-10-28 17:00:14 +0200

    close #45 Añadiendo una primera versión de la validación de los ingredientes. Por ahora es básica y valida lo principal

--- src/recetas.py
+++ src/recetas.py
@@ -1,6 +1,6 @@
 class Receta:
 
-	unidadesPermitidas = {"gramos", "gr", "cucharada", "cucharadita", "ralladura", "diente", "dientes"}
+	unidadesPermitidas = ["litro", "litros", "kg", "kilos", "kilo", "kg", "gramos", "gr", "cucharada", "cucharadas", "cucharadita", "cucharaditas", "ralladura", "ralladuras", "diente", "dientes"]
 	
 	def __init__(self, nombreRecetas, alimentos, elaboracion, tiempo):
 		self.nombreRecetas = nombreRecetas
@@ -9,6 +9,17 @@ class Receta:
 		self.tiempo = tiempo
 
 
-	def validarAlimentos(self, receta, unidadesPermitidas):
-		return None
-
+	def validarAlimentos(receta, unidadesPermitidas):
+		conjAlimentos = receta.alimentos
+		alimentos1 = conjAlimentos.split(",")
+		contador = 0
+		for i in range(0, len(alimentos1)):
+			alimentos2 = alimentos1[i].split(" ")
+			for j in range(0, len(alimentos2)):
+				for l in range(0, len(unidadesPermitidas)):
+					if(alimentos2[j] == unidadesPermitidas[l]):
+						contador=contador+1
+		if(contador == len(alimentos1)):
+			return True
+		else:
+			return False
