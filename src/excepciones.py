97786b91f3f610a8b04a36ec44f5f62fcc7d562b
Juan Carlos Gonzalez Quesada <juancarlosgq@correo.ugr.es>
2021-10-29 09:17:37 +0200

    close #55 Añado las excepciones para el nombre

--- src/excepciones.py
+++ src/excepciones.py
@@ -4,3 +4,18 @@ class Error(Exception):
 class RecetaSinNombre(Error):
 	pass
 
+	def nombre_incorrecto(nombre):
+		if(nombre == ""):
+			return True
+		else:
+			return False
+
+	def validar_nombre(nombre_recetas):
+		try:
+			if RecetaSinNombre.nombre_incorrecto(nombre_recetas):
+				raise RecetaSinNombre
+			else:
+				print("NOMBRE CORRECTO!")
+		except RecetaSinNombre:
+			print("ERROR: No se ha introducido un nombre valido")
+			exit()
