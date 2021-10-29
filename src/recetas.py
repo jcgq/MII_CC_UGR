e4428a6f98985903bbe4b626e1a61753109f4ab6
Juan Carlos Gonzalez Quesada <juancarlosgq@correo.ugr.es>
2021-10-29 09:05:09 +0200

    close #52 Añado el objeto valor alimentos a la Clase Receta

--- src/recetas.py
+++ src/recetas.py
@@ -1,25 +1,16 @@
+from excepciones import RecetaSinNombre
+from funciones import *
+
 class Receta:
 
-	unidadesPermitidas = ["litro", "litros", "kg", "kilos", "kilo", "kg", "gramos", "gr", "cucharada", "cucharadas", "cucharadita", "cucharaditas", "ralladura", "ralladuras", "diente", "dientes"]
-	
-	def __init__(self, nombreRecetas, alimentos, elaboracion, tiempo):
-		self.nombreRecetas = nombreRecetas
+	diccionario_unidades = ["litro", "litros", "kg", "kilos", "kilo", 
+	"kg", "gramos", "gr", "cucharada", "cucharadas", "cucharadita", 
+	"cucharaditas", "ralladura", "ralladuras", "diente", "dientes"]
+
+	diccionario_alimentos = obtener_diccionario_alimentos()
+
+	def __init__(self, nombre_recetas, alimentos, elaboracion, tiempo):
+		self.nombreRecetas = nombre_recetas
 		self.alimentos = alimentos
 		self.elaboracion = elaboracion
 		self.tiempo = tiempo
-
-
-	def validarAlimentos(receta, unidadesPermitidas):
-		conjAlimentos = receta.alimentos
-		alimentos1 = conjAlimentos.split(",")
-		contador = 0
-		for i in range(0, len(alimentos1)):
-			alimentos2 = alimentos1[i].split(" ")
-			for j in range(0, len(alimentos2)):
-				for l in range(0, len(unidadesPermitidas)):
-					if(alimentos2[j] == unidadesPermitidas[l]):
-						contador=contador+1
-		if(contador == len(alimentos1)):
-			return True
-		else:
-			return False
