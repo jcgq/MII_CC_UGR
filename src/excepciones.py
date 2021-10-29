ad47584a8ee3ebc325afff2b77ec51a945e6faa1
Juan Carlos Gonzalez Quesada <juancarlosgq@correo.ugr.es>
2021-10-29 09:32:03 +0200

    close #56 Añado la excepción que permite comprobar si la lista de alimentos es correcta

--- src/excepciones.py
+++ src/excepciones.py
@@ -5,7 +5,7 @@ class RecetaSinNombre(Error):
 	pass
 
 	def nombre_incorrecto(nombre):
-		if(nombre == ""):
+		if(nombre == "" or len(nombre)<3):
 			return True
 		else:
 			return False
@@ -19,3 +19,25 @@ class RecetaSinNombre(Error):
 		except RecetaSinNombre:
 			print("ERROR: No se ha introducido un nombre valido")
 			exit()
+
+	def alimentos_incorrectos(alimentos, unidades_permitidas, diccionario_alimentos):
+		alimentos = alimentos.split(";")
+		correcto=True
+		for i in range(0, len(alimentos)):
+			print(alimentos[i])
+			if not any(alimento in alimentos[i] for alimento in diccionario_alimentos):
+				correcto = False
+			if not any(unidad in alimentos[i] for unidad in unidades_permitidas):
+				correcto = False
+		return correcto
+
+	def validar_alimentos(alimentos, unidades_permitidas, diccionario_alimentos):
+		try:
+			if not RecetaSinNombre.alimentos_incorrectos(alimentos, unidades_permitidas, diccionario_alimentos):
+				raise RecetaSinNombre
+			else:
+				print("ALIMENTOS CORRECTOS!")
+		except RecetaSinNombre:
+			print("ERROR: No se ha introducido un alimento valido")
+			exit()
+
