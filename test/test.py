0b7cadb17490c6c9bb648c480f60db5f373dc4e0
Juan Carlos Gonzalez Quesada <juancarlosgq@correo.ugr.es>
2021-10-29 10:15:38 +0200

    Arreglando el test erróneo

--- test/test.py
+++ test/test.py
@@ -4,8 +4,8 @@ sys.path.insert(1, 'src')
 from recetas import Receta
 
 def test_validarCapacidades():
-    recetaCorrecta = Receta("Caracoles", "1 kilo de habichuelas, medio litro de agua, dos ralladuras de melón","Cocinar todo con mucho tranquiilp", 67)
-    assert(Receta.validarAlimentos(recetaCorrecta, Receta.unidadesPermitidas) == True)
-    
+    recetaCorrecta = Receta("huevo frito", "un kilo de patatas; cien gramos de jamón", "Con mucho esmero y arte, se cocina un huevo", 22)    
+    assert(isinstance(recetaCorrecta, Receta))
+
     recetaIncorrecta = Receta("Caracoles", "1 carabinero de habichuelas, medio litro de agua, dos cazuelicas de melón","Cocinar todo con mucho tranquiilp", 67)
-    assert(Receta.validarAlimentos(recetaIncorrecta, Receta.unidadesPermitidas) == False)   
+    assert(isinstance(recetaIncorrecta, Receta))
