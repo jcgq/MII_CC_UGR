ef2845525a66958ffc4fad23b9555d8780d45532
Juan Carlos Gonzalez Quesada <juancarlosgq@correo.ugr.es>
2021-10-29 10:00:19 +0200

    close #51 añado las líneas necesarias para testear el constructor, mostrar excepciones si hay errores y crear un objeto correcto

--- src/recetas.py
+++ src/recetas.py
@@ -1,4 +1,4 @@
-from excepciones import RecetaSinNombre
+from excepciones import *
 from funciones import *
 
 class Receta:
@@ -10,7 +10,13 @@ class Receta:
 	diccionario_alimentos = obtener_diccionario_alimentos()
 
 	def __init__(self, nombre_recetas, alimentos, elaboracion, tiempo):
+		RecetaSinNombre.validar_nombre(nombre_recetas)
 		self.nombreRecetas = nombre_recetas
+		RecetaSinAlimentos.validar_alimentos(alimentos, Receta.diccionario_unidades, Receta.diccionario_alimentos["alimentos"])
 		self.alimentos = alimentos
+		RecetaSinElaboracion.validar_elaboracion(elaboracion)
 		self.elaboracion = elaboracion
+		RecetaSinTiempo.validar_tiempo(tiempo)
 		self.tiempo = tiempo
+
+Receta("huevo frito", "un kilo de patatas; cien gramos de jamón", "Con mucho esmero y arte, se cocina un huevo", 22)
