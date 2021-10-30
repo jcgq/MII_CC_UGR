e98f8199ffbc847fa5387a27027e2377e311513a
Juan Carlos Gonzalez Quesada <juancarlosgq@correo.ugr.es>
2021-10-30 10:24:35 +0200

    close #56 Añadiendo el test para la función que valida los alimentos

--- test/test.py
+++ test/test.py
@@ -16,3 +16,25 @@ def test_nombre():
     #Test correcto. El nombre cumple con los requisitos
     nombre = "huevo frito"
     assert(nombre_incorrecto(nombre)==False)
+
+def test_alimentos():
+    diccionario_unidades = obtener_diccionario_unidades()
+    diccionario_alimentos = obtener_diccionario_alimentos()
+
+    #Test erróneo. Los alimentos están vacíos o con longitud errónea
+    alimento = ""
+    assert(alimentos_incorrectos(alimento, diccionario_unidades, diccionario_alimentos["alimentos"])==True)
+    alimento = "un pan"
+    assert(alimentos_incorrectos(alimento, diccionario_unidades, diccionario_alimentos["alimentos"])==True)
+
+    #Test erróneo. Los alimentos tienen alimentos no permitidos
+    alimento = "un kilo de sanjacobos;un gramo de aire"
+    assert(alimentos_incorrectos(alimento, diccionario_unidades, diccionario_alimentos["alimentos"])==True)
+
+    #Test erróneo. Los alimentos tienen unidades no permitidas
+    alimento = "una cazuela de champiñones; una onza de chocolate"
+    assert(alimentos_incorrectos(alimento, diccionario_unidades, diccionario_alimentos["alimentos"])==True)
+
+    #Test correcto. Los alimentos cumplen con los requisitos
+    alimento = "un kilo de champiñones; cien gramos de chocolate"
+    assert(alimentos_incorrectos(alimento, diccionario_unidades, diccionario_alimentos["alimentos"])==False)
