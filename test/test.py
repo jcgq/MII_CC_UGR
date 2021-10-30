7637f1c3b5915f66464eeebd6ce07a8148769b27
Juan Carlos Gonzalez Quesada <juancarlosgq@correo.ugr.es>
2021-10-30 11:29:15 +0200

    close #51 Añadiendo el test para la función que valida todos los atributos de la receta

--- test/test.py
+++ test/test.py
@@ -68,3 +68,43 @@ def test_tiempo():
     #Test correcto. El tiempo cumple con los requisitos
     tiempo = 78
     assert(tiempo_incorrecto(tiempo)==False)
+
+
+def test_validacion_completa():
+    diccionario_unidades = obtener_diccionario_unidades()
+    diccionario_alimentos = obtener_diccionario_alimentos()
+
+    #Test incorrecto. El nombre no es válido
+    nombre = ""
+    alimentos= "un kilo de champiñones; cien gramos de chocolate"
+    elaboracion = "Hay que remover todo con la espátula y que el aceite esté bien caliente"
+    tiempo = 78
+    assert(validar_caracteristicas_receta(nombre, alimentos, diccionario_unidades, diccionario_alimentos["alimentos"], elaboracion, tiempo)==True)
+    
+    #Test incorrecto. Los alimentos no son válidos
+    nombre = "Huevo frito"
+    alimentos= "un amor de champiñones; cien gramos de chocolate"
+    elaboracion = "Hay que remover todo con la espátula y que el aceite esté bien caliente"
+    tiempo = 78
+    assert(validar_caracteristicas_receta(nombre, alimentos, diccionario_unidades, diccionario_alimentos["alimentos"], elaboracion, tiempo)==True)
+
+    #Test incorrecto. La elaboración no es válida
+    nombre = ""
+    alimentos= "un kilo de champiñones; cien gramos de chocolate"
+    elaboracion = "Nada"
+    tiempo = 78
+    assert(validar_caracteristicas_receta(nombre, alimentos, diccionario_unidades, diccionario_alimentos["alimentos"], elaboracion, tiempo)==True)
+
+    #Test incorrecto. El tiempo no es válido
+    nombre = ""
+    alimentos= "un kilo de champiñones; cien gramos de chocolate"
+    elaboracion = "Hay que remover todo con la espátula y que el aceite esté bien caliente"
+    tiempo = "hola"
+    assert(validar_caracteristicas_receta(nombre, alimentos, diccionario_unidades, diccionario_alimentos["alimentos"], elaboracion, tiempo)==True)
+
+    #Test correcto. Se puede crear el objeto receta
+    nombre = "Huevo frito"
+    alimentos= "un kilo de champiñones; cien gramos de chocolate"
+    elaboracion = "Hay que remover todo con la espátula y que el aceite esté bien caliente"
+    tiempo = 45
+    assert(validar_caracteristicas_receta(nombre, alimentos, diccionario_unidades, diccionario_alimentos["alimentos"], elaboracion, tiempo)==False)
