ac30b1808f99bb2c391df16782c4444a02422ed8
Juan Carlos Gonzalez Quesada <juancarlosgq@correo.ugr.es>
2021-11-17 15:19:43 +0100

    #83 Añado setup.py
    Archivo para instalar los módulos evitando el uso de pip

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(
+    name='requerimientos',
+    description='Instalación de módulos necesarios',
+    license="MIT",
+    author='Juan Carlos González Quesada',
+    install_requires=['pyTest', 'googletrans', 'invoke', 'nltk', 'sklearn']
+)
