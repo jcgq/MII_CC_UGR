7773342489ffcd221b1e9b097ab4774723656320
Juan Carlos Gonzalez Quesada <juancarlosgq@correo.ugr.es>
2021-11-22 12:44:50 +0100

    #3 close #81 Test constructor [skip ci]
    Testeando el constructor de las recetas para evitar objetos inválidos

--- test/test.py
+++ test/test.py
@@ -155,6 +155,36 @@ class Test(unittest.TestCase):
         assert(Receta.obtener_puntuacion(0.7,elaboraciones) == True)
 
 
+    def test_constructor_comparacion(self):
+        #Test Incorrecto. La receta ya está en el sistema
+        nombre = "Huevo frito"
+        alimentos= "1 kilo de champiñones;100 gramos de chocolate"
+        elaboracion = "Cómo hacer un buen sofrito de paella Poned primero la paella o recipiente a fuego fuerte con aceite de oliva y un poco de sal para que no salpique la carne, añadid la carne (si estáis haciendo paella de marisco el marisco) y doradla. Luego bajad el fuego y añadimos las verduras, dejando que se rehoguen durante unos 5 minutos. Veréis que en este tipo de sofrito para paella no se usa cebolla, ya que los jugos de la misma abren el arroz. El arroz ideal para la paella valenciana El arroz es el principal ingrediente de la paella valenciana, y conseguir un arroz en su punto es el mejor secreto para que vuestra paella brille con luz propia. En alguna ocasión os hemos explicado los distintos tipos de arroz que existen, y en el caso de la paella valenciana el mejor arroz es el arroz bomba, que tiene un grano medio-corto y cuya principal característica es que absorbe más agua que otros arroces. El arroz bomba es más caro que otros arroces pero os aseguramos que la inversión merece la pena ¡vuestra paella ganará muchos puntos!"
+        tiempo = "23"
+        receta = None
+        try:
+            receta = Receta(nombre, alimentos, elaboracion, tiempo)
+        except:
+            assert(receta == None)
+
+        #Test Incorrecto. La receta es muy similar a una en el sistema
+        nombre = "Huevo frito"
+        alimentos= "1 kilo de champiñones;100 gramos de chocolate"
+        elaboracion = "Cómo hacer un buen sofrito de paella Poned primero la paella a fuego fuerte con aceite de oliva y un poco de sal para que no salpique la carne, añadid el pescado y doradla. Luego bajad el fuego y añadimos las verduras, dejando que se rehoguen durante unos 5 minutos. Veréis que en este tipo de sofrito para paella no se usa cebolla, ya que los jugos de la misma abren el arroz. El arroz ideal para la paella valenciana El arroz es el principal ingrediente de la paella valenciana, y conseguir un arroz en su punto es el mejor secreto para que vuestra paella brille con luz propia. En alguna ocasión os hemos explicado los distintos tipos de arroz que existen, y en el caso de la paella valenciana el mejor arroz es el arroz bomba, que tiene un grano medio-corto y cuya principal característica es que absorbe más agua que otros arroces. El arroz bomba es más caro que otros arrocitos pero os aseguramos que la inversión merece la pena"
+        tiempo = "23"
+        receta = None
+        try:
+            receta = Receta(nombre, alimentos, elaboracion, tiempo)
+        except:
+            assert(receta == None)
+
+        #Test correcto. Se crea la receta
+        nombre = "Huevo frito"
+        alimentos= "1 kilo de champiñones;100 gramos de chocolate"
+        elaboracion = "Hay que remover todo con la espátula y que el aceite esté bien caliente"
+        tiempo = "23"
+        receta = Receta(nombre, alimentos, elaboracion, tiempo)
+        assert(receta.__class__ == Receta)
 
 
 
