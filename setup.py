72618417c626ff63e266368ebae611a126680aba
Juan Carlos Gonzalez Quesada <juancarlosgq@correo.ugr.es>
2021-11-27 13:32:37 +0100

    #87 Prueba
    Añado librerías y cambio la versión

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     description='Instalación de módulos necesarios',
     license="MIT",
     author='Juan Carlos González Quesada',
-    install_requires=['pyTest', 'googletrans', 'invoke', 'nltk', 'sklearn']
+    install_requires=['numpy', 'pybind11', 'Cython', 'pyTest', 'googletrans', 'invoke', 'scipy', 'nltk', 'sklearn']
 )
