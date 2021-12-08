f253dec4b5ddf4294382c41bddbf5d15fa268c14
Juan Carlos Gonzalez Quesada <juancarlosgq@correo.ugr.es>
2021-12-08 11:07:39 +0100

    #88 Primeros pasos [skip ci]
    Creación de la app por defecto y uso de la clase Confg

--- recetarium/api.py
+++ recetarium/api.py
@@ -1,5 +1,15 @@
+import requests
 import bottle
-from bottle import run
+import os
+from bottle import get, post, run, route, response, error, request
+import json
+from funciones import *
+from recetas import *
+from urllib.parse import unquote
+from configuracion import Configuracion
+
+confg = Configuracion()
+app = bottle.default_app()
 
 if __name__ == "__main__":
-    run(host='127.0.0.0', port='8000', debug=False, reloader=True)
+    run(host=confg.host, port=confg.puerto, debug=False, reloader=True)
