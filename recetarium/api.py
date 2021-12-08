40e94769e35231cd6de57359e9230b1dcbf645f0
Juan Carlos Gonzalez Quesada <juancarlosgq@correo.ugr.es>
2021-12-08 11:23:53 +0100

    #88 Rutas [skip ci]
    Primeras rutas y corrección de la configuración

--- recetarium/api.py
+++ recetarium/api.py
@@ -16,5 +16,28 @@ logging.basicConfig(filename='fichero.log',
                     format='%(asctime)s : %(levelname)s : %(message)s',
                     filemode='w', level=logging.INFO)
 
+#Página de inicio
+@route('/')
+def recetas_contador():
+	logging.info("Estás en la página de inicio")
+	response.status = 204
+	return '{"Bienvenid@!" : "A mi API de Recetas"}'
+
+@get('/recetas/<nombre>')
+def receta(nombre):
+	json_alimentos = obtener_json()
+	diccionario = obtener_diccionario(json_alimentos)
+	receta = diccionario.get(nombre.lower())
+
+	if receta == None:
+		logging.error('Error. No existe la receta en el sistema')
+		response.status = 404
+		return "{\"Error\":\"404 Receta no encontrada\"}"
+	else:
+		receta = json.dumps(receta, indent=4, sort_keys=True)
+		logging.info('Éxito. La receta existe y se devuelve al usuario.')
+		response.status=200
+		return receta
+
 if __name__ == "__main__":
     run(host=confg.host, port=confg.puerto, debug=False, reloader=True)
