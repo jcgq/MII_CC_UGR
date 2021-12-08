e4c0c8aa91ce1f868003cabdb21ad8209b5514ce
Juan Carlos Gonzalez Quesada <juancarlosgq@correo.ugr.es>
2021-12-08 11:13:13 +0100

    #88 #92 Inicialización de los logs [skip ci]
    Utilización y creación de los logs

--- recetarium/api.py
+++ recetarium/api.py
@@ -7,9 +7,14 @@ from funciones import *
 from recetas import *
 from urllib.parse import unquote
 from configuracion import Configuracion
+import logging
 
 confg = Configuracion()
 app = bottle.default_app()
+
+logging.basicConfig(filename='fichero.log',
+                    format='%(asctime)s : %(levelname)s : %(message)s',
+                    filemode='w', level=logging.INFO)
 
 if __name__ == "__main__":
     run(host=confg.host, port=confg.puerto, debug=False, reloader=True)
