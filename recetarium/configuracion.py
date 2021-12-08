170eec1c4f0b13b0ee2983b4009146602863f768
Juan Carlos Gonzalez Quesada <juancarlosgq@correo.ugr.es>
2021-12-08 11:04:58 +0100

    #91 Configuración distribuida [skip ci]
    Creación de la clase y el constructor básico

---
+++ recetarium/configuracion.py
@@ -0,0 +1,7 @@
+class Configuracion():
+    def __init__(self):
+        self.puerto = "8000"
+        self.host = "127.0.0"
+        self.ruta_recetas = "./json/recetas.json"
+        self.rutas_alimentos = "./json/alimentos_en.json"
+
